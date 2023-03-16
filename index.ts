4151889aa0f6539b6cd51bb66c50696244d6df4c
Daniela Farias <farias.danicris@gmail.com>
2023-03-15 23:58:23 -0300

    feat: movies array

--- index.ts
+++ index.ts
@@ -2,39 +2,38 @@ const express = require("express");
 
 const server = express();
 
+const movies = [
+  {
+    backdrop_path: "/AsEgGeccI32SwMBkxpwhOkhbzmF.jpg",
+    id: 1,
+    overview:
+      "Décadas atrás, a URSS desenvolveu tubarões impossíveis de matar e os lançou à lua. Hoje, uma equipe de astronautas americanos enfrentará a luta de suas vidas.",
+    poster_path: "/v5CfpzxoJDkZxjZAizClFdlEF0U.jpg",
+    title: "Shark Side of the Moon",
+    vote_average: 5.3,
+  },
+  {
+    backdrop_path: "/22z44LPkMyf5nyyXvv8qQLsbom.jpg",
+    id: 2,
+    overview:
+      "Durante as férias em uma cabana remota, uma jovem e seus pais são feitos reféns por quatro estranhos armados que exigem que a família faça uma escolha impensável para evitar o apocalipse. Com acesso limitado ao mundo exterior, a família deve decidir no que acredita antes que tudo esteja perdido.",
+    poster_path: "/xtLEm7icyupihsdhUYXJdQ7sKFo.jpg",
+    title: "Batem à Porta",
+    vote_average: 6.4,
+  },
+];
+
 server.get("/movies", (req, res) => {
   // /movies?test=testando
-  const testQuery = req.query.test;
+//   const testQuery = req.query.test;
 
-  return res.json([
-    {
-      backdrop_path: "/22z44LPkMyf5nyyXvv8qQLsbom.jpg",
-      id: 631842,
-      overview:
-        "Durante as férias em uma cabana remota, uma jovem e seus pais são feitos reféns por quatro estranhos armados que exigem que a família faça uma escolha impensável para evitar o apocalipse. Com acesso limitado ao mundo exterior, a família deve decidir no que acredita antes que tudo esteja perdido.",
-      poster_path: "/xtLEm7icyupihsdhUYXJdQ7sKFo.jpg",
-      title: "Batem à Porta",
-      vote_average: 6.4,
-      query: `${testQuery}`,
-    },
-  ]);
+  return res.json(movies);
 });
 
 server.get("/movies/:id", (req, res) => {
-// /movies/631842
   const id = req.params.id;
 
-  return res.json(
-    {
-      backdrop_path: "/22z44LPkMyf5nyyXvv8qQLsbom.jpg",
-      id,
-      overview:
-        "Durante as férias em uma cabana remota, uma jovem e seus pais são feitos reféns por quatro estranhos armados que exigem que a família faça uma escolha impensável para evitar o apocalipse. Com acesso limitado ao mundo exterior, a família deve decidir no que acredita antes que tudo esteja perdido.",
-      poster_path: "/xtLEm7icyupihsdhUYXJdQ7sKFo.jpg",
-      title: "Batem à Porta",
-      vote_average: 6.4,
-    },
-  );
+  return res.json(movies[id]);
 });
 
 server.listen(3000);
