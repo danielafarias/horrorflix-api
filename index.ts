d5c09ea3638f35a29f692629394a9c74a04291b5
Daniela Farias <farias.danicris@gmail.com>
2023-03-15 23:36:22 -0300

    initialize project

---
+++ index.ts
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const server = express();
+
+server.get("/movies", (req, res) => {
+  return res.json([
+    {
+      backdrop_path: "/22z44LPkMyf5nyyXvv8qQLsbom.jpg",
+      id: 631842,
+      overview:
+        "Durante as férias em uma cabana remota, uma jovem e seus pais são feitos reféns por quatro estranhos armados que exigem que a família faça uma escolha impensável para evitar o apocalipse. Com acesso limitado ao mundo exterior, a família deve decidir no que acredita antes que tudo esteja perdido.",
+      poster_path: "/xtLEm7icyupihsdhUYXJdQ7sKFo.jpg",
+      title: "Batem à Porta",
+      vote_average: 6.4,
+    },
+  ]);
+});
+
+server.listen(3000);
